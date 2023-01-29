e4ebbb5b0d26902fe8f33048c267e328124db90d
Denis Carriere <carriere.denis@gmail.com>
2023-01-29 16:43:53 -0500

    cargo fmt

--- src/maps.rs
+++ src/maps.rs
@@ -3,10 +3,7 @@ use substreams::errors::Error;
 use substreams::{log, prelude::*};
 
 #[substreams::handlers::map]
-pub fn map_counters(
-    store_traces_count: Deltas<DeltaInt64>,
-    store_action_count: Deltas<DeltaInt64>,
-) -> Result<Counters, Error> {
+pub fn map_counters(store_traces_count: Deltas<DeltaInt64>, store_action_count: Deltas<DeltaInt64>) -> Result<Counters, Error> {
     let mut counters = Vec::new();
 
     for delta in store_traces_count.deltas {
