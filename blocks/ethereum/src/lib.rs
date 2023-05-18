2df9129a0f65a9c2795bdb1f9519fc9af9247310
ali-sultani <sultani.alishoja@gmail.com>
2023-05-18 09:34:09 -0400

    Implementation of map_block_stats for ethereum

--- blocks/ethereum/src/lib.rs
+++ blocks/ethereum/src/lib.rs
@@ -2,6 +2,8 @@
 #[allow(dead_code)]
 pub mod pb;
 pub use pb::*;
+use std::collections::HashSet;
+use substreams::Hex;
 
 use substreams::errors::Error;
 use substreams_ethereum::pb::eth::v2::Block;
@@ -9,13 +11,16 @@ use substreams_ethereum::pb::eth::v2::Block;
 #[substreams::handlers::map]
 pub fn map_block_stats(block: Block) -> Result<BlockStats, Error> {
     let mut trace_calls: i64 = 0;
+    let mut uaw = HashSet::new();
+
     for trace in block.transaction_traces.clone() {
         trace_calls += trace.calls.len() as i64;
+        uaw.insert(Hex(&trace.from).to_string());
     }
 
     Ok(BlockStats {
         transaction_traces: block.transaction_traces.len() as i64,
         trace_calls,
-        uaw: Vec::new(), // TO-DO
+        uaw: uaw.into_iter().collect(), 
     })
 }
