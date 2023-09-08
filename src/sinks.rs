9d2f6ad345246d9569fec4c1390a334136f14fb1
Charles Morin <charles.mor@outlook.fr>
2023-09-08 14:21:18 -0400

    Removed daw from prom_out

--- src/sinks.rs
+++ src/sinks.rs
@@ -1,14 +1,10 @@
-use std::borrow::BorrowMut;
-use std::collections::HashMap;
 use substreams::errors::Error;
-use substreams::store::StoreAddBigInt;
-use substreams::{log, pb::substreams::Clock};
-use substreams_sink_prometheus::{Counter, Gauge, PrometheusOperations};
+use substreams_sink_prometheus::{Counter, PrometheusOperations};
 
 use crate::pb::BlockStats;
 
 #[substreams::handlers::map]
-pub fn prom_out(stats: BlockStats, clock: Clock) -> Result<PrometheusOperations, Error> {
+pub fn prom_out(stats: BlockStats) -> Result<PrometheusOperations, Error> {
     let mut prom_out = PrometheusOperations::default();
 
     if stats.trace_calls > 0 {
@@ -18,23 +14,6 @@ pub fn prom_out(stats: BlockStats, clock: Clock) -> Result<PrometheusOperations,
     if stats.transaction_traces > 0 {
         prom_out.push(Counter::from("transaction_traces").add(stats.transaction_traces as f64));
     }
-
-    // TO-DO
-    // push the daily unique wallets to prometheus
-    let seconds = clock.timestamp.unwrap().seconds;
-    let epoch = (seconds / 86400) * 86400;
-
-    let day: i64 = epoch / 86400;
-    let value: u8 = 1;
-    let mut labels: HashMap<String, String> = HashMap::new();
-
-    for wallet in stats.uaw.iter() {
-        let key = format!("daw:{}:{}", day, wallet);
-        labels.insert(key, value.to_string());
-    }
-
-    let mut gauge = Gauge::from("daw").with(labels);
-    prom_out.push(gauge.set(1.0));
 
     Ok(prom_out)
 }
