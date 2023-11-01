7bb64019198eda56a98209d03529195cfd3dd149
Denis <carriere.denis@gmail.com>
2023-11-01 12:35:43 -0400

    Update sinks.rs

--- src/sinks.rs
+++ src/sinks.rs
@@ -6,25 +6,9 @@ use crate::pb::BlockStats;
 #[substreams::handlers::map]
 pub fn graph_out(stats: BlockStats) -> Result<EntityChanges, Error> {
     let mut tables = Tables::new();
-    let mut key = "".to_string();
-    for wallet in stats.uaw.iter() {
-        key = format!("daw:{}", wallet);
-    }
-    let row =  tables
-    .create_row("BlockStats", key);
-    if stats.trace_calls > 0 {
-        row.set("trace_calls", stats.trace_calls);
-    }
-    else{
-        row.set("trace_calls", 0);
-    }
-
-    if stats.transaction_traces > 0 {
-        row.set("transaction_traces", stats.transaction_traces);
-    }
-    else{
-        row.set("transaction_traces", 0);
-    }
+    row.set("uaw", stats.uaw);
+    row.set("trace_calls", stats.trace_calls);
+    row.set("transaction_traces", stats.transaction_traces);
 
     Ok(tables.to_entity_changes())
 }
