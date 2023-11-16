1878c6b5278766580f397cba712985e814b97ba0
Denis Carriere <carriere.denis@gmail.com>
2023-11-16 15:10:24 -0500

    update block_id

--- schema.sql
+++ schema.sql
@@ -1,8 +1,7 @@
 CREATE TABLE IF NOT EXISTS BlockStats  (
-    id String,
     transaction_traces Int64,
     trace_calls  Int64,
     uaw  Array(String)
 )
 ENGINE = ReplacingMergeTree()
-PRIMARY KEY (id)
+ORDER BY (block_id)
